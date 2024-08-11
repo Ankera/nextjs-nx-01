1d33e08fbf63e235eb83e9aa320b7efdad667b92
ankera <15189120919@163.com>
2024-08-11 15:03:57 +0800

    feat: 公共头部和尾部

--- apps/details/src/app/[slug]-c_[catId]/page.tsx
+++ apps/details/src/app/[slug]-c_[catId]/page.tsx
@@ -1,3 +1,5 @@
+import { Header, Footer } from '@myorg/components';
+
 type PageProps = {
   params: {
     slug: string;
@@ -6,13 +8,19 @@ type PageProps = {
 };
 
 export default function DynamicPage({ params }: PageProps) {
+  console.log('==params==', params);
   const { slug, catId } = params;
 
   return (
     <div>
+      <Header title='这是头部' />
       <h1>Dynamic Page</h1>
       <p>Slug: {slug}</p>
       <p>Category ID: {catId}</p>
+      <pre>
+        {JSON.stringify(params)}
+      </pre>
+      <Footer />
     </div>
   );
 }
--- apps/details/src/app/page.tsx
+++ apps/details/src/app/page.tsx
@@ -1,8 +1,11 @@
 import Link from 'next/link';
+import { Header, Footer } from '@myorg/components';
 
 export default function Details() {
   return (
     <div>
+      <Header title="这是商品详情页公共头部" />
+
       <h1>Welcome to Details Page</h1>
       <p>
         This is the details page.
@@ -10,6 +13,8 @@ export default function Details() {
       <Link href="http://localhost:3000">
           Go to Home Page
       </Link>
+
+      <Footer />
     </div>
   );
 }
